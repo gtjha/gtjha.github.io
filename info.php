369d72f9c93e23df6dc9b635065c9f4b4c0efda5
gtjha <gautamjha405@gmail.com>
2017-04-25 12:47:01 +0530

    new files

--- info.php
+++ info.php
@@ -1,3 +1,22 @@
-<?php
 
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "info submitted";
 ?>
+<p style="color:blue; font-size:160%;"><b>All buying options</b></p>
+<p>buy through state bank credit card</p>
+<a href="cod.html">
+    <img src="cr.png"  width="250" height="142">
+  </a>
+<p>buy through punjab netional bank credit card</p>
+<a href="cod.html">
+    <img src="pn.bmp" width="250" height="142">
+</a>
+<a href="cod.html"></br></br>cash on delihvary</a>
+</body>
+</html>
+
